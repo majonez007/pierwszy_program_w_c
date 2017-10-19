6b96813227ad1356d7de5fd7dbc07527c4f7283a
majonez007 <maj0nez@poczta.fm>
2017-10-19 16:08:24 +0200

    pierwszy program zakonczony

---
+++ program.c
@@ -0,0 +1,16 @@
+#include <stdio.h> /* dolacz biblioteke standardowa input-output */
+
+
+/* y = f(x) */
+
+/* main() => 0 	*/
+
+int main ()	/* funkcja glowna programu, zwraca wartosc calkowita */
+{ /* miedzy klamrami jest cialo fukcji*/
+
+	puts("Pierwszy program"); /* funkcja z biblioteki stdio */
+
+	return 0;	/* main zwraca wrtosc 0*/
+
+}
+
